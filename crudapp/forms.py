d6956511a87d0f4d78373b169d8135115db34228
kritamd <kritam1@gmail.com>
2021-05-29 18:23:38 +0530

    Django-CRUD Operation

---
+++ crudapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from crudapp.models import User
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields="__all__"
+        widgets={
+            'uname': forms.TextInput(attrs={'class': 'form-control'}),
+            'uemail':forms.TextInput(attrs={'class':'form-control'}),
+            'upassword':forms.PasswordInput(attrs={'class':'form-control'})
+        }
